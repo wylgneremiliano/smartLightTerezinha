0a6844faa7565e09ddaf7b5fd4099f626ed86868
sergioSouza <sergio_souza.s@hotmail.com>
2025-09-16 22:06:32 -0300

    feat: reorganiza componente de filtro com novos estilos e estrutura

--- src/pages/list/components/filter/index.tsx
+++ src/pages/list/components/filter/index.tsx
@@ -3,9 +3,19 @@ import "./style.css";
 
 const Filter = () => {
   return (
-    <div className="filter">
-      <h1>Filtros</h1>
-      <img className="iconeFilter" src={filter} />
+    <div className="filter-Container">
+      <div className="filter-header">
+        <h1>Filtros</h1>
+        <img className="iconeFilter" src={filter} />
+      </div>
+
+      <div className="description-header">
+        <h2>Tipo</h2>
+        <h2>Nome</h2>
+        <h2>Grupo</h2>
+        <h2>Status de Conexão</h2>
+        <h2>Ligar/Desligar</h2>
+      </div>
     </div>
   );
 };
