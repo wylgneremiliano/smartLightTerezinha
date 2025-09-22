0f8f7cfcf89f4f46feac9027d1457d49801d9a4b
Sergio Souza da Silva <sergio_souza,s@hotmail.com>
2025-09-22 17:18:19 -0300

    feat: atualiza estilos e estrutura da listagem com novas linhas de dados

--- src/pages/list/components/listing/index.tsx
+++ src/pages/list/components/listing/index.tsx
@@ -2,6 +2,7 @@ import "./style.css";
 
 const Listing = () => {
   return (
+    <div className="list"> 
     <div className="description-Header">
       <h2>Tipo</h2>
       <h2>Nome</h2>
@@ -9,6 +10,49 @@ const Listing = () => {
       <h2>Status de Conexão</h2>
       <h2>Ligar/Desligar</h2>
     </div>
+
+    <div className="table-line">
+      <div>Lâmpada</div>
+      <div>Lâmpada_sala_01</div>
+      <div>Sala</div>
+      <div>Conectado/Desconectado</div>
+      <input type="checkbox"></input>
+      </div>
+
+    <div className="table-line">  
+      <div>Lâmpada</div>
+      <div>Lâmpada_sala_02</div>
+      <div>Sala</div>
+      <div>Conectado/Desconectado</div>
+      <input type="checkbox"></input>
+      </div>
+
+    <div className="table-line">
+      <div>Lâmpada</div>
+      <div>Lâmpada_sala_03</div>
+      <div>Sala</div>
+      <div>Conectado/Desconectado</div>
+      <input type="checkbox"></input>
+      </div>
+
+    <div className="table-line">
+      <div>Lâmpada</div>
+      <div>Lâmpada_sala_04</div>
+      <div>Sala</div>
+      <div>Conectado/Desconectado</div>
+      <input type="checkbox"></input>
+      </div>
+
+    <div className="table-line">
+      <div>Lâmpada</div>
+      <div>Lâmpada_sala_05</div>
+      <div>Sala</div>
+      <div>Conectado/Desconectado</div>
+      <input type="checkbox"></input>
+      </div>
+  
+    </div>
+  
   );
 };
 
