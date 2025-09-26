9ab048802e76b3f661543fd8019730e98de754df
sergioSouza <sergio_souza.s@hotmail.com>
2025-09-25 22:20:38 -0300

    feat: integrate MUI components and refactor listing page
    - Added dependencies for Emotion and MUI in package.json.
    - Refactored the Listing component to dynamically render items from a JSON mock data file.
    - Replaced checkbox inputs with MUI Switch components for better UI consistency.
    - Updated styles to reflect new component structure and added color classes for connection status.
    - Created a mock data file (lista-itens.json) to simulate item data for the listing.

--- src/pages/list/components/listing/index.tsx
+++ src/pages/list/components/listing/index.tsx
@@ -1,58 +1,30 @@
 import "./style.css";
+import listaItens from "../../../../mock/lista-itens.json";
+import { Switch } from "@mui/material";
 
 const Listing = () => {
   return (
-    <div className="list"> 
-    <div className="description-Header">
-      <h2>Tipo</h2>
-      <h2>Nome</h2>
-      <h2>Grupo</h2>
-      <h2>Status de Conexão</h2>
-      <h2>Ligar/Desligar</h2>
+    <div className="list">
+      <div className="description-Header">
+        <h2>Tipo</h2>
+        <h2>Nome</h2>
+        <h2>Grupo</h2>
+        <h2>Status de Conexão</h2>
+        <h2>Ligar/Desligar</h2>
+      </div>
+      {listaItens.map((item) => (
+        <div className="table-line">
+          <div>{item.tipo}</div>
+          <div>{item.nome}</div>
+          <div>{item.grupo}</div>
+          <div className={`${item.status_conexao ? "green" : "gray"}`}>
+            {item.status_conexao ? "Conectado" : "Desconectado"}
+          </div>
+          <Switch color="success" checked={item.estado} />
+        </div>
+      ))}
+    
     </div>
-
-    <div className="table-line">
-      <div>Lâmpada</div>
-      <div>Lâmpada_sala_01</div>
-      <div>Sala</div>
-      <div>Conectado/Desconectado</div>
-      <input type="checkbox"></input>
-      </div>
-
-    <div className="table-line">  
-      <div>Lâmpada</div>
-      <div>Lâmpada_sala_02</div>
-      <div>Sala</div>
-      <div>Conectado/Desconectado</div>
-      <input type="checkbox"></input>
-      </div>
-
-    <div className="table-line">
-      <div>Lâmpada</div>
-      <div>Lâmpada_sala_03</div>
-      <div>Sala</div>
-      <div>Conectado/Desconectado</div>
-      <input type="checkbox"></input>
-      </div>
-
-    <div className="table-line">
-      <div>Lâmpada</div>
-      <div>Lâmpada_sala_04</div>
-      <div>Sala</div>
-      <div>Conectado/Desconectado</div>
-      <input type="checkbox"></input>
-      </div>
-
-    <div className="table-line">
-      <div>Lâmpada</div>
-      <div>Lâmpada_sala_05</div>
-      <div>Sala</div>
-      <div>Conectado/Desconectado</div>
-      <input type="checkbox"></input>
-      </div>
-  
-    </div>
-  
   );
 };
 
