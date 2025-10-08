52c97bb6ce505903e22c60fbe2e37605f893ac34
sergioSouza <sergio_souza.s@hotmail.com>
2025-10-07 22:15:44 -0300

    feat: remove ícone de filtro e implementa caixa de busca com estilos atualizados

--- src/pages/list/components/filter/index.tsx
+++ src/pages/list/components/filter/index.tsx
@@ -1,16 +1,19 @@
-import filter from "../../../../assets/filterIcon.png";
 import "./style.css";
+import buscar from "../../../../assets/buscar.png";
 
 const Filter = () => {
   return (
     <div className="filter-Container">
       <div className="filter-Header">
-        <h1>Filtros</h1>
-        <img className="iconeFilter" src={filter} />
+        <div className="caixa-busca">
+          <img className="img-busca" src={buscar}></img>
+          <input className="buscar" placeholder="Buscar"></input>
+        </div>
+
+        </div>
+        
       </div>
 
-    
-    </div>
   );
 };
 
